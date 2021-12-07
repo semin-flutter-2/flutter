791b5cfd4080e64c3f5a38e9adaacb889553384d
Junsuk Oh <a811219@gmail.com>
2021-12-07 11:29:22 +0900

    BottomNavigationBar 적용

--- kakao_t_ui_exam/lib/ui/home/home_screen.dart
+++ kakao_t_ui_exam/lib/ui/home/home_screen.dart
@@ -1,86 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:kakao_t_ui_exam/data/fake_data.dart';
-import 'package:kakao_t_ui_exam/model/ad.dart';
-import 'package:kakao_t_ui_exam/ui/home/components/ad_view.dart';
-import 'package:kakao_t_ui_exam/ui/home/components/menu_widget.dart';
+import 'package:kakao_t_ui_exam/ui/counter/counter_screen.dart';
+import 'package:kakao_t_ui_exam/ui/kakao_t/kakao_t_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _screens = [
+    KakaoTScreen(),
+    CounterScreen(),
+  ];
+
+  int _index = 0;
+
+  @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          '카카오 T',
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      body: _buildBody(),
-    );
-  }
-
-  Widget _buildBody() {
-    final PageController controller = PageController(initialPage: 0);
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: ListView(
-        children: [
-          _buildMenu(),
-          _buildAds(controller),
-          _buildNotice(),
+      body: _screens[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Kakao T',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.watch),
+            label: '카운터',
+          ),
         ],
       ),
     );
   }
-
-  Widget _buildMenu() {
-    return GridView.count(
-      crossAxisCount: 4,
-      shrinkWrap: true,
-      childAspectRatio: 2.5 / 3.2,
-      physics: NeverScrollableScrollPhysics(),
-      children: fakeMenus.map((e) => MenuWidget(menu: e)).toList(),
-    );
-  }
-
-  Widget _buildAds(PageController controller) {
-    return SizedBox(
-          height: 150,
-          child: PageView(
-            scrollDirection: Axis.horizontal,
-            controller: controller,
-            children: fakeAds.map((Ad e) => AdView(ad: e)).toList(),
-            // children: <Widget>[
-            //   AdView(
-            //     ad: fakeAds[0],
-            //   ),
-            //   AdView(
-            //     ad: fakeAds[1],
-            //   ),
-            //   AdView(
-            //     ad: fakeAds[2],
-            //   ),
-            // ],
-          ),
-        );
-  }
-
-  Widget _buildNotice() {
-    // ListView 보다 Column만 두는게 더 깔끔하다
-    return ListView(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      children: List.generate(50, (index) => ListTile(
-        leading: Icon(Icons.notifications),
-        title: Text('공지 $index'),
-        trailing: Icon(
-          Icons.navigate_next_outlined,
-        ),
-      )),
-    );
-  }
 }
-
---
+++ kakao_t_ui_exam/lib/ui/kakao_t/kakao_t_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:kakao_t_ui_exam/data/fake_data.dart';
+import 'package:kakao_t_ui_exam/model/ad.dart';
+
+import 'components/ad_view.dart';
+import 'components/menu_widget.dart';
+
+
+class KakaoTScreen extends StatelessWidget {
+  const KakaoTScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          '카카오 T',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    final PageController controller = PageController(initialPage: 0);
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ListView(
+        children: [
+          _buildMenu(),
+          _buildAds(controller),
+          _buildNotice(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMenu() {
+    return GridView.count(
+      crossAxisCount: 4,
+      shrinkWrap: true,
+      childAspectRatio: 2.5 / 3.2,
+      physics: NeverScrollableScrollPhysics(),
+      children: fakeMenus.map((e) => MenuWidget(menu: e)).toList(),
+    );
+  }
+
+  Widget _buildAds(PageController controller) {
+    return SizedBox(
+          height: 150,
+          child: PageView(
+            scrollDirection: Axis.horizontal,
+            controller: controller,
+            children: fakeAds.map((Ad e) => AdView(ad: e)).toList(),
+            // children: <Widget>[
+            //   AdView(
+            //     ad: fakeAds[0],
+            //   ),
+            //   AdView(
+            //     ad: fakeAds[1],
+            //   ),
+            //   AdView(
+            //     ad: fakeAds[2],
+            //   ),
+            // ],
+          ),
+        );
+  }
+
+  Widget _buildNotice() {
+    // ListView 보다 Column만 두는게 더 깔끔하다
+    return ListView(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      children: List.generate(50, (index) => ListTile(
+        leading: Icon(Icons.notifications),
+        title: Text('공지 $index'),
+        trailing: Icon(
+          Icons.navigate_next_outlined,
+        ),
+      )),
+    );
+  }
+}
+
