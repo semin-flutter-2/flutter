769e00f950dc4ce7d199982deb3bdebc545a4ee0
Junsuk Oh <a811219@gmail.com>
2021-12-13 16:31:12 +0900

    BMI 계산기

--- kakao_t_ui_exam/lib/ui/bmi_calc/bmi_calc_home_screen.dart
+++ kakao_t_ui_exam/lib/ui/bmi_calc/bmi_calc_home_screen.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'bmi_calc_result_screen.dart';
 
-class BmiCalcHomeScreen extends StatelessWidget {
+class BmiCalcHomeScreen extends StatefulWidget {
   const BmiCalcHomeScreen({Key key}) : super(key: key);
+
+  @override
+  State<BmiCalcHomeScreen> createState() => _BmiCalcHomeScreenState();
+}
+
+class _BmiCalcHomeScreenState extends State<BmiCalcHomeScreen> {
+  final _heightTextEditingController = TextEditingController();
+  final _weightTextEditingController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _heightTextEditingController.dispose();
+    _weightTextEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,23 +29,65 @@ class BmiCalcHomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('BMI 계산기'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(),
-            TextField(),
-            ElevatedButton(
-              onPressed: () {
-                // 버튼 눌렀을 때
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => BmiCalcResultScreen()),
-                );
-              },
-              child: Text('계산'),
-            ),
-          ],
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _heightTextEditingController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '키',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '키를 입력하세요';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: _weightTextEditingController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '몸무게',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '몸무게를 입력하세요';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+              ),
+              ButtonBar(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        // 버튼 눌렀을 때
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BmiCalcResultScreen(
+                                double.parse(_heightTextEditingController.text.trim()),
+                                double.parse(_weightTextEditingController.text.trim()),
+                              )),
+                        );
+                      }
+                    },
+                    child: Text('계산'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- kakao_t_ui_exam/lib/ui/bmi_calc/bmi_calc_result_screen.dart
+++ kakao_t_ui_exam/lib/ui/bmi_calc/bmi_calc_result_screen.dart
@@ -1,27 +1,71 @@
 import 'package:flutter/material.dart';
 
 class BmiCalcResultScreen extends StatelessWidget {
-  const BmiCalcResultScreen({Key key}) : super(key: key);
+  final double height;
+  final double weight;
+
+  const BmiCalcResultScreen(this.height, this.weight, {Key key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final bmi = weight / ((height / 100) * (height / 100));
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('결과'),
-      ),
+      appBar: AppBar(title: Text('비만도 계산기')),
       body: Center(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Text(
-            '결과 화면',
-            style: TextStyle(
-              fontSize: 30
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _calcBmi(bmi),
+              style: TextStyle(fontSize: 36),
             ),
-          ),
+            SizedBox(
+              height: 16,
+            ),
+            _buildIcon(bmi),
+          ],
         ),
       ),
     );
+  }
+
+  String _calcBmi(double bmi) {
+    var result = '저체중';
+    if (bmi >= 35) {
+      result = '고도 비만';
+    } else if (bmi >= 30) {
+      result = '2단계 비만';
+    } else if (bmi >= 25) {
+      result = '1단계 비만';
+    } else if (bmi >= 23) {
+      result = '과체중';
+    } else if (bmi >= 18.5) {
+      result = '정상';
+    }
+    return result;
+  }
+
+  Widget _buildIcon(double bmi) {
+    if (bmi >= 23) {
+      return Icon(
+        Icons.sentiment_very_dissatisfied,
+        color: Colors.red,
+        size: 100,
+      );
+    } else if (bmi >= 18.5) {
+      return Icon(
+        Icons.sentiment_satisfied,
+        color: Colors.green,
+        size: 100,
+      );
+    } else {
+      return Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.orange,
+        size: 100,
+      );
+    }
   }
 }
