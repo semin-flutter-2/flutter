3883f7b75c72828e8cc541c825f3a784b04d02a3
Junsuk Oh <a811219@gmail.com>
2021-12-14 12:50:32 +0900

    이미지 검색 앱 1

---
+++ image_search/lib/ui/home/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:image_search/data/fake_data.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('이미지 검색'),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: fakePhotos
+              .map((e) => Image.network(e.previewURL))
+              .toList(),
+        ));
+  }
+}
