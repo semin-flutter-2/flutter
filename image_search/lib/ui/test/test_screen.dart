d321ccef887cd90ef206946ba2e2f5a02be85df5
Junsuk Oh <a811219@gmail.com>
2021-12-20 11:13:12 +0900

    네트워크 통신 기초 3
    setState() 호출하여 FutureBuilder 다시 로드 가능

--- image_search/lib/ui/test/test_screen.dart
+++ image_search/lib/ui/test/test_screen.dart
@@ -4,9 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_search/model/album.dart';
 
-class TestScreen extends StatelessWidget {
+class TestScreen extends StatefulWidget {
   const TestScreen({Key? key}) : super(key: key);
 
+  @override
+  State<TestScreen> createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen> {
   // 오래 걸리는 처리
   Future<Album> fetchAlbum() async {
     // await [Future가 리턴되는 코드]
@@ -30,27 +35,37 @@ class TestScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Network Sample'),
       ),
-      body: FutureBuilder<Album>(
-        future: fetchAlbum(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('네트워크 에러!!!'));
-          }
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              setState(() {});
+            },
+            child: const Text('Album 가져오기'),
+          ),
+          FutureBuilder<Album>(
+            future: fetchAlbum(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Center(child: Text('네트워크 에러!!!'));
+              }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
 
-          // 데이터가 없다면
-          if (!snapshot.hasData) {
-            return const Center(child: Text('데이터가 없습니다'));
-          }
+              // 데이터가 없다면
+              if (!snapshot.hasData) {
+                return const Center(child: Text('데이터가 없습니다'));
+              }
 
-          // 데이터가 여기에서는 무조건 있는 상황
-          final Album album = snapshot.data!;
+              // 데이터가 여기에서는 무조건 있는 상황
+              final Album album = snapshot.data!;
 
-          return _buildBody(album);
-        },
+              return _buildBody(album);
+            },
+          ),
+        ],
       ),
     );
   }
