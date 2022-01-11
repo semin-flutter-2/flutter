b6749b1713e664b79b74c8204aa5144110bfc3b8
Junsuk Oh <a811219@gmail.com>
2022-01-11 14:16:40 +0900

    ViewModel의 상태를 캡슐화

--- counter_provider/lib/ui/home/home_view_model.dart
+++ counter_provider/lib/ui/home/home_view_model.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HomeViewModel with ChangeNotifier {
-  int counter = 0;
+  int _counter = 0;
+
+  int get counter => _counter;
 
   void increment() {
     // 상태 바꾸고
-    counter++;
+    _counter++;
 
     notifyListeners();
   }
