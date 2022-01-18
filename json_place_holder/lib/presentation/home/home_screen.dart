a396bd36c6e9a60dd1f5d0be4ae1b59df2230ff8
Junsuk Oh <a811219@gmail.com>
2022-01-18 15:56:03 +0900

    잘 되는지 테스트 성공

--- json_place_holder/lib/presentation/home/home_screen.dart
+++ json_place_holder/lib/presentation/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:json_place_holder/presentation/home/home_view_model.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,16 +14,28 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
 
+    Future.microtask(() {
+      _updateUi(1);
+    });
+  }
 
+  Future<void> _updateUi(int page) async {
+    final viewModel = context.read<HomeViewModel>();
+    viewModel.fetch(page);
   }
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<HomeViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Posts'),
       ),
-      body: Container(),
+      body: viewModel.isLoading
+          ? const CircularProgressIndicator()
+          : ListView(
+              children: viewModel.posts.map((e) => Text('${e.id}')).toList(),
+            ),
     );
   }
 }
