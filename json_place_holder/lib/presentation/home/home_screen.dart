9990bf77b636069daf390cbce4bc01804c14ee3f
Junsuk Oh <a811219@gmail.com>
2022-01-18 15:49:15 +0900

    Provider 설정

---
+++ json_place_holder/lib/presentation/home/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:json_place_holder/presentation/home/home_view_model.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Posts'),
+      ),
+      body: Container(),
+    );
+  }
+}
